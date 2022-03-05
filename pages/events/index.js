66bd273733d1e81804774c68dbb0fb0175f5e6a1
hnee <rornfdlek@naver.com>
2022-03-05 16:53:32 +0900

    83. Module Summary

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,26 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from 'next/router';
+
+
 function AllEventsPage() {
-    return <div>
-        <h1>The All Events Page</h1>
-    </div>
+
+    const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
+    return (
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler} />
+            <EventList items={events} />
+        </Fragment>
+    );
 }
 
 export default AllEventsPage;
