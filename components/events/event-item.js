5ba419c23c990785a36946bea68548d67b5dd510
hnee <rornfdlek@naver.com>
2022-03-09 16:08:29 +0900

    135. Taking A Look At The Next Image Documentation

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Button from "../ui/button";
+import Image from "next/image";
 
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
@@ -21,7 +22,7 @@ function EventItem(props) {
 
     return (
         <li className={classes.item}>
-            <img src={'/' + image} alt={title} />
+            <Image src={'/' + image} alt={title} width={250} height={160} />
             <div className={classes.content}>
                 <div className={classes.summary}>
                     <h2>{title}</h2>
