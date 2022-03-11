413c6da23f7ba2ac84f66bfbeb0ac7f3ed52d306
hnee <rornfdlek@naver.com>
2022-03-11 15:54:45 +0900

    157. Inserting Comments Into The Database

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,6 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+async function handler(req, res) {
 
     const eventId = req.query.eventId;
+
+    dotenv.config();
+    const client = await MongoClient.connect(`mongodb+srv://rornfdlek:${process.env.PASSWORD}@cluster0.vyy0p.mongodb.net/events?retryWrites=true&w=majority`);
 
     if(req.method === 'POST') {
         const {email, name, text} = req.body; 
@@ -12,12 +18,19 @@ function handler(req, res) {
         }
 
         const newComment = {
-            id: new Date().toISOString(),
             email,
             name,
-            text
-        }
-        console.log(newComment);
+            text,
+            eventId
+        };
+
+        const db = client.db();
+        const result = await db.collection('comments').insertOne({newComment});
+
+        console.log(result);
+
+        newComment.id = result.insertedId;
+
         res.status(201).json({message: 'Added Comment!', comment: newComment})
     }
 
@@ -28,6 +41,7 @@ function handler(req, res) {
         ];
         res.status(200).json({comments: dummyList});
     }
+    client.close();
 }
 
 export default handler;
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -13,9 +13,9 @@ async function handler(req, res) {
 
         dotenv.config();
     
-        const client = await MongoClient.connect(`mongodb+srv://rornfdlek:${process.env.PASSWORD}@cluster0.vyy0p.mongodb.net/newsletter?retryWrites=true&w=majority`);
+        const client = await MongoClient.connect(`mongodb+srv://rornfdlek:${process.env.PASSWORD}@cluster0.vyy0p.mongodb.net/events?retryWrites=true&w=majority`);
         const db = client.db();
-        await db.collection('emails').insertOne({email: userEmail});
+        await db.collection('newsletter').insertOne({email: userEmail});
         client.close();
         
         res.status(201).json({message: "Signed Up!"})
