88728e615d12a3e28eefd1a404ba6dab9c236447
hnee <rornfdlek@naver.com>
2022-03-11 15:45:32 +0900

    156. Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+async function handler(req, res) {
     if(req.method === 'POST') {
         const userEmail = req.body.email;
         
@@ -8,8 +11,14 @@ function handler(req, res) {
             return;
         }
 
-        console.log(userEmail);
-        res.status(201).json({message: 'Signed Up'})
+        dotenv.config();
+    
+        const client = await MongoClient.connect(`mongodb+srv://rornfdlek:${process.env.PASSWORD}@cluster0.vyy0p.mongodb.net/newsletter?retryWrites=true&w=majority`);
+        const db = client.db();
+        await db.collection('emails').insertOne({email: userEmail});
+        client.close();
+        
+        res.status(201).json({message: "Signed Up!"})
     }
 }
 
