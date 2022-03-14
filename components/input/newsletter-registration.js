3c17e12084405eaa5bc6da5d9eddbc3dce9deec7
hnee <rornfdlek@naver.com>
2022-03-14 18:12:54 +0900

    170. Example: Triggering & Showing Notifications

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,9 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
 
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
@@ -14,15 +16,42 @@ function NewsletterRegistration() {
 
     const enteredEmail = emailInputRef.current.value;
 
+    notificationCtx.showNotification({
+      title: "Signing Up...",
+      message: "Registering for Newsletter...",
+      status: "pending"
+    });
+
     fetch('/api/newsletter', {
       method: 'POST',
-      body: JSON.stringify({email: enteredEmail}),
+      body: JSON.stringify({ email: enteredEmail }),
       headers: {
         'Content-Type': 'application/json'
       }
     })
-    .then(response => response.json())
-    .then(data => console.log(data));
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Successfully Registered for Newsletter",
+          status: "success"
+        });
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || 'Something went wrong!',
+          status: 'error'
+        });
+      });
   }
 
   return (
